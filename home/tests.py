5a25da7702338f9b51bb60be41c5887b1b612870
Chinseok Lee <me@askcompany.kr>
2019-04-22 13:08:49 +0900

    유닛테스트 추가

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,19 @@
+from django.shortcuts import resolve_url
 from django.test import TestCase
 
-# Create your tests here.
+
+class HomeTest(TestCase):
+    def setUp(self):
+        self.page_url = resolve_url("home:index")
+
+    def test_url(self):
+        self.assertEqual(self.page_url, "/")
+
+    def test_status_code_200(self):
+        res = self.client.get(self.page_url)
+        self.assertEqual(res.status_code, 200)
+
+    def test_title(self):
+        res = self.client.get(self.page_url)
+        self.assertContains(res, 'Azure DevOps 배포 데모')
+
